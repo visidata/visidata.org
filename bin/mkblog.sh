f188a8637a672ab443c33a0a8a0f97c5f393cf76
anjakefala <anja.kefala@gmail.com>
2019-12-27 20:33:22 -0500

    [bin] ensure all bash scripts are shellcheck compliant

--- bin/mkblog.sh
+++ bin/mkblog.sh
@@ -3,21 +3,19 @@
 set -e
 
 WWWSRC=$(pwd)
-BLOGWWW=$WWWSRC/blog
+BLOG=$WWWSRC/blog
 BUILD=$WWWSRC/_blog
 
 BIN=$WWWSRC/bin
 
 TMPDIR=$(mktemp -d)
 
-mkdir -p $BUILD
+mkdir -p "$BUILD"
 
-cp -R $WWWSRC/static/404.html $BUILD/
-cp -R $WWWSRC/static/favicon.ico $BUILD/
-cp -R $WWWSRC/static/robots.txt $BUILD/
-cp -R $WWWSRC/static/vdlogo.png $BUILD/
-cp -R $WWWSRC/static/main.css $BUILD/
+for fn in 404.html favicon.ico robots.txt vdlogo.png main.css; do
+    cp -R "$WWWSRC"/static/"$fn" "$BUILD"/
+done
 
 source "$BIN"/mkpage.sh
 
-build_page . $BLOGWWW/index.md "Blog"
+build_page . "$BLOG"/index.md "Blog"
--- bin/mkwww.sh
+++ bin/mkwww.sh
@@ -4,14 +4,15 @@ set -e  # stop script on error
 
 ### config env vars
 
-# SRC is already cloned visidata repo, with no modifications (other tags/branches will be checked out)
-SRC=`pwd`/visidata
-
 # WWWSRC is already cloned visidata.org repo, with desired branch to build checked out
-WWWSRC=`pwd`
+WWWSRC=$(pwd)
+
+# SRC is already cloned visidata repo, with no modifications (other tags/branches will be checked out)
+SRC="$WWWSRC"/visidata
+export PYTHONPATH=$SRC:$SRC/visidata
 
 # BUILD is output directory (corresponding to webroot)
-BUILD=$WWWSRC/_build
+BUILD="$WWWSRC"/_build
 
 # VERSIONS (visidata release tags) to generate /docs/vX.Y.Z/
 #    /docs itself will be branch already checked out
@@ -19,14 +20,13 @@ VERSIONS="v1.0 v1.1 v1.2 v1.3 v1.4 v1.5 develop"   # should be populated from ta
 
 ### internal env vars
 
-TMPDIR=`mktemp -d`
-BIN=$WWWSRC/bin
-export PYTHONPATH=$SRC:$SRC/visidata
+TMPDIR=$(mktemp -d)
+BIN="$WWWSRC"/bin
 
-mkdir -p $BUILD
+mkdir -p "$BUILD"
 
 # STATICFILES=404.html robots.txt main.css devotees.gpg.key vdlogo.png screenshot.png
-cp -R $WWWSRC/static/* $BUILD/
+cp -R "$WWWSRC"/static/* "$BUILD"/
 
 source "$BIN"/mkpage.sh
 
@@ -35,56 +35,52 @@ function build_docs() {
     docdir=$BUILD/$1
 
     # build manpage
-    mkdir -p $BUILD/man
-    manhtml=$TMPDIR/vd-man-inc.html
-    echo '<section><pre id="manpage">' > $manhtml
+    mkdir -p "$BUILD"/man
+    manhtml="$TMPDIR"/vd-man-inc.html
+    echo '<section><pre id="manpage">' > "$manhtml"
     # <pre> max-width in main.css should be half of COLUMNS=###
-    MAN_KEEP_FORMATTING=1 COLUMNS=1000 man $SRC/visidata/man/vd.1 | ul | aha --no-header >> $manhtml
-    echo '</pre></section>' >> $manhtml
-    $BIN/strformat.py body=$manhtml title="VisiData Quick Reference" head="" < $WWWSRC/template.html > $BUILD/man/index.html
+    MAN_KEEP_FORMATTING=1 COLUMNS=1000 man "$SRC"/visidata/man/vd.1 | ul | aha --no-header >> "$manhtml"
+    echo '</pre></section>' >> "$manhtml"
+    "$BIN"/strformat.py body="$manhtml" title="VisiData Quick Reference" head="" < "$WWWSRC"/template.html > "$BUILD"/man/index.html
 
-    # Create /man/#loaders
-    sed -i -e "s#<span style=\"font-weight:bold;\">SUPPORTED</span> <span style=\"font-weight:bold;\">SOURCES</span>#<span style=\"font-weight:bold;\"><a name=\"loaders\">SUPPORTED SOURCES</a></span>#g" $BUILD/man/index.html
-    # Create /man#edit for editing commands
-    sed -i -e "s#<span style=\"font-weight:bold;\">Editing</span> <span style=\"font-weight:bold;\">Rows</span> <span style=\"font-weight:bold;\">and</span> <span style=\"font-weight:bold;\">Cells</span>#<span style=\"font-weight:bold;\"><a name=\"edit\">Editing Rows and Cells</a></span>#g" $docdir/man/index.html
     # Create /man#options
-    sed -i -e "s#<span style=\"font-weight:bold;\">COMMANDLINE</span> <span style=\"font-weight:bold;\">OPTIONS</span>#<span style=\"font-weight:bold;\"><a name=\"options\">OPTIONS</a></span>#g" $BUILD/man/index.html
+    sed -i -e "s#<span style=\"font-weight:bold;\">COMMANDLINE</span> <span style=\"font-weight:bold;\">OPTIONS</span>#<span style=\"font-weight:bold;\"><a name=\"options\">OPTIONS</a></span>#g" "$BUILD"/man/index.html
     # Create /man#columns for columns sheet
-    sed -i -e "s#<span style=\"font-weight:bold;\">Columns</span> <span style=\"font-weight:bold;\">Sheet</span> <span style=\"font-weight:bold;\">(Shift-C)</span>#<span style=\"font-weight:bold;\"><a name=\"columns\">Columns Sheet (Shift-C)</a></span>#g" $BUILD/man/index.html
+    sed -i -e "s#<span style=\"font-weight:bold;\">Columns</span> <span style=\"font-weight:bold;\">Sheet</span> <span style=\"font-weight:bold;\">(Shift-C)</span>#<span style=\"font-weight:bold;\"><a name=\"columns\">Columns Sheet (Shift-C)</a></span>#g" "$BUILD"/man/index.html
 
     # build kblayout
-    mkdir -p $docdir/kblayout
-    $BIN/mklayout.py $WWWSRC/template.html $SRC/visidata/commands.tsv > $docdir/kblayout/index.html
-    cp $WWWSRC/static/kblayout.css $docdir/kblayout/
+    mkdir -p "$docdir"/kblayout
+    "$BIN"/mklayout.py "$WWWSRC"/template.html "$SRC"/visidata/commands.tsv > "$docdir"/kblayout/index.html
+    cp "$WWWSRC"/static/kblayout.css "$docdir"/kblayout/
 
     # build index
     if [ -d "$SRC/docs" ]; then
         SRCDOCS=$SRC/docs
-        build_page $1 $SRCDOCS/index.md "VisiData Documentation"
+        build_page "$1" "$SRCDOCS"/index.md "VisiData Documentation"
     else
         # up until v1.4 docs were located in $SRC/www/docs
         SRCDOCS=$SRC/www/docs
-        build_page $1 $SRC/www/docs.md "VisiData Documentation"
+        build_page "$1" "$SRC"/www/docs.md "VisiData Documentation"
     fi
 
-    for postpath in `find $SRCDOCS -name '*.md'`; do
+    for postpath in $(find $SRCDOCS -name '*.md'); do
         post=${postpath##$SRCDOCS}
         postname=${post%.md}
-        build_page $1/$postname $postpath "$postname"
+        build_page "$1"/"$postname" "$postpath" "$postname"
     done
 
-    mkdir -p $docdir/casts
-    cp $SRCDOCS/casts/* $docdir/casts
+    mkdir -p "$docdir"/casts
+    cp "$SRCDOCS"/casts/* "$docdir"/casts
 }
 
-build_page . $WWWSRC/index.md "VisiData"
-build_page install $WWWSRC/install.md "Installation Instructions"
-build_page privacy $WWWSRC/privacy.md "Privacy Policy"
-build_page releases $WWWSRC/releases.md "Release History"
-build_page credits $WWWSRC/credits.md "Credits and Contributions"
-build_page formats $WWWSRC/formats.md "Supported Sources and Formats"
-build_page v2.x $WWWSRC/v2.x.md "VisiData version 2.0"
-build_page podcast $WWWSRC/podcast-transcript.md "Podcast"
+build_page . "$WWWSRC"/index.md "VisiData"
+build_page install "$WWWSRC"/install.md "Installation Instructions"
+build_page privacy "$WWWSRC"/privacy.md "Privacy Policy"
+build_page releases "$WWWSRC"/releases.md "Release History"
+build_page credits "$WWWSRC"/credits.md "Credits and Contributions"
+build_page formats "$WWWSRC"/formats.md "Supported Sources and Formats"
+build_page v2.x "$WWWSRC"/v2.x.md "VisiData version 2.0"
+build_page podcast "$WWWSRC"/podcast-transcript.md "Podcast"
 
 
 set +e
@@ -95,7 +91,7 @@ build_docs docs
 for ver in $VERSIONS ; do
     echo "Building /docs/$ver"
 
-    cd $SRC && git checkout $ver
-    build_docs docs/$ver
+    cd "$SRC" && git checkout "$ver"
+    build_docs docs/"$ver"
 done
 
