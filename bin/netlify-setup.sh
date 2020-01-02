c5af692750f4ef24d586ec89c8be12f80b0a8d79
anjakefala <anja.kefala@gmail.com>
2020-01-02 15:09:51 -0800

    [aha] add tar to install dir for installation on netlify

--- bin/netlify-setup.sh
+++ bin/netlify-setup.sh
@@ -2,7 +2,6 @@ installdir=$(pwd)/install
 mkdir -p "$installdir"
 
 # for building the manpage
-wget http://deb.debian.org/debian/pool/main/a/aha/aha_0.5.orig.tar.gz -O "$installdir"/aha_0.5.orig.tar.gz
 tar -xzf "$installdir"/aha_0.5.orig.tar.gz
 cd aha-0.5 || exit
 make
