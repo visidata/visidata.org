325c30345a2ec79abd10f172dcc889d05bba8c28
anjakefala <anja.kefala@gmail.com>
2020-01-02 15:15:12 -0800

    [netlify] add buildhome/.local to path

--- bin/netlify-setup.sh
+++ bin/netlify-setup.sh
@@ -2,6 +2,7 @@ installdir=$(pwd)/install
 mkdir -p "$installdir"
 
 # for building the manpage
+export PATH=$PATH:/opt/buildhome/.local/bin
 tar -xzf "$installdir"/aha_0.5.orig.tar.gz
 cd aha-0.5 || exit
 make
