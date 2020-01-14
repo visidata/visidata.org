0c226c483e326b8cefc3b60dc63dbac7b8840d99
anjakefala <anja.kefala@gmail.com>
2020-01-13 22:31:52 -0800

    [build] remove aha from build process

--- bin/netlify-setup.sh
+++ bin/netlify-setup.sh
@@ -1,12 +1,1 @@
-installdir=$(pwd)/install
-mkdir -p "$installdir"
-
-# for building the manpage
-export PATH=$PATH:/opt/buildhome/.local/bin
-tar -xzf "$installdir"/aha_0.5.orig.tar.gz
-cd aha-0.5 || exit
-make
-make install PREFIX=/opt/buildhome/.local
-
-
 bin/mkwww.sh
