2b3ac8977d021820cb9363133885f2ae7f23bea1
Luke <luke@luke.work>
2021-06-07 22:11:40 -0500

    brand colors

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,32 +1,36 @@
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: [
-      "site/**/*.html",
-      "site/**/*.njk",
-      "site/**/*.md"
-    ],
-    options: {
-      whitelist: [],
-    },
-  },
-  theme: {
-    extend: {
-        colors: {
-          change: "transparent"
+    mode: 'jit',
+    purge: {
+        content: [
+            "site/**/*.html",
+            "site/**/*.njk",
+            "site/**/*.md"
+        ],
+        options: {
+            whitelist: [],
         },
-          animation: {
-            fadeInDown: "fadeInDown 1s ease-in-out forwards"
-          },
-          keyframes: {
-            fadeInDown: {
-              "0%": { opacity: 0, transform: 'translateY(-50%)'},
-              "100%": { opacity: 1, transform: 'translateY(0)'}
+    },
+    theme: {
+        extend: {
+            colors: {
+                change: "transparent",
+                brand: {
+                    primary: "#A300FF",
+                    dark: "#2C0044"
+                }
+            },
+            animation: {
+                fadeInDown: "fadeInDown 1s ease-in-out forwards"
+            },
+            keyframes: {
+                fadeInDown: {
+                    "0%": { opacity: 0, transform: 'translateY(-50%)' },
+                    "100%": { opacity: 1, transform: 'translateY(0)' }
+                }
             }
-          }
-    }
+        }
     },
     variants: {},
     plugins: [],
     darkMode: 'media',
-  }
+}
