fe6702b9126a263dde22506ceba7487c20d50b43
Luke <luke@luke.work>
2021-06-25 16:48:45 -0500

    script output to html files; these aren't templates so no need for .njk

--- lukeone.sh
+++ lukeone.sh
@@ -3,7 +3,7 @@
 SRCDOCS=visidata/docs/
 post=${1##$SRCDOCS}
 postname=${post%.md}
-OUTFN=site/docs/$postname.njk
+OUTFN=site/docs/$postname.html
 echo $OUTFN
 ./parsefrontmatter.py $1 $OUTFN $OUTFN-rest.md
 pandoc --from markdown_strict+table_captions+header_attributes+implicit_header_references+simple_tables+fenced_code_blocks+pipe_tables+fenced_divs -w html $OUTFN-rest.md -o - >> $OUTFN
