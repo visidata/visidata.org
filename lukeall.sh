daa4dc22eeb9ab6a781f5bdac46111fef1d109f0
Luke Harris <luke+git@lkhrs.com>
2023-04-12 09:29:02 -0500

    [docs] combine bash scripts all for one, one for all

--- lukeall.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for fn in visidata/docs/*.md ; do
-    ./lukeone.sh $fn
-done
--- mkdocs.sh
+++ mkdocs.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-SRCDOCS=visidata/docs/
-post=${1##$SRCDOCS}
+SRCDOCS=./visidata/docs/
+
+for fn in $SRCDOCS*.md ; do
+post=${fn##$SRCDOCS}
 postname=${post%.md}
 OUTFN=site/docs/$postname.html
 echo $OUTFN
-./parsefrontmatter.py $1 $OUTFN $OUTFN-rest.md
+./parsefrontmatter.py $fn $OUTFN $OUTFN-rest.md
 pandoc --from markdown_strict+table_captions+header_attributes+implicit_header_references+simple_tables+fenced_code_blocks+pipe_tables+fenced_divs -w html $OUTFN-rest.md -o - >> $OUTFN
+done
