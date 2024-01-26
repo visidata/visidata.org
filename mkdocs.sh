ff7c04c70871059c0ec344e98e1aa583db059c5e
Saul Pwanson <code@saul.pw>
2024-01-25 22:50:59 -0800

    [docs] can use "[[GUIDE GuideName]]" to include formatted guide text

--- mkdocs.sh
+++ mkdocs.sh
@@ -8,6 +8,6 @@ post=${fn##$SRCDOCS}
 postname=${post%.md}
 OUTFN=$OUTDIR/$postname.html
 echo $OUTFN
-./parsefrontmatter.py $fn $OUTFN $OUTFN-rest.md
-pandoc --from markdown_strict+table_captions+header_attributes+implicit_header_references+simple_tables+fenced_code_blocks+pipe_tables+fenced_divs+footnotes -w html $OUTFN-rest.md -o - >> $OUTFN
+./processdoc.py $fn $OUTFN $OUTFN-processed.md
+pandoc --from markdown_strict+table_captions+header_attributes+implicit_header_references+simple_tables+fenced_code_blocks+pipe_tables+fenced_divs+footnotes -w html $OUTFN-processed.md -o - >> $OUTFN
 done
